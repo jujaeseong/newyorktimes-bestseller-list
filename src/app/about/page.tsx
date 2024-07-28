093d2e71d76acb5b264bc0677409e8ed5f2c0c4c
Jaeseong Ju <devfeac@gmail.com>
2024-07-28 23:01:29 +0900

    apply about page css

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,14 @@
+import styles from "./about.module.css";
+
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.title}>ABOUT US</h1>
+      <p>
+        Welcome to the official explorer for The New York Best Sellers list
+        explorer.
+      </p>
+      <p>We hope you enjoy your stay!</p>
+    </section>
+  );
 }
