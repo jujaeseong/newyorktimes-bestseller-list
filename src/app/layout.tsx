cd60d3ea1351314a44371a36599db198d541129b
Jaeseong Ju <devfeac@gmail.com>
2024-07-28 22:30:23 +0900

    set font and apply navigation css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
+import "./global.css";
+import { Patrick_Hand } from "next/font/google";
 import Navigation from "@/components/Navigation";
 import { Metadata } from "next";
+
+const patrickHand = Patrick_Hand({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "New York Times Best Sellers",
@@ -13,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={patrickHand.className}>
         <Navigation />
         {children}
       </body>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,18 +2,31 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "./navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
 
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
         <li>
-          <Link href="/">Home</Link>
+          <Link
+            href="/"
+            className={`${styles.link} ${path === "/" ? styles.active : ""}`}
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link
+            href="/about"
+            className={`${styles.link} ${
+              path === "/about" ? styles.active : ""
+            }`}
+          >
+            About
+          </Link>
         </li>
       </ul>
     </nav>
